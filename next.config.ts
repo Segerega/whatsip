60c2335d8d3d011a96756bdb32d24da8db40c201
Sergey M <smy@avrillo.co.uk>
2024-11-04 14:42:35 +0000

    Deploy

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-    distDir: 'dist', // Set the build output to './dist'
+    distDir: '.next',
 };
 
 export default nextConfig;
